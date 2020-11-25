14987be86e3b79450b97ba20c52dd11475c35948
Blazej <master@constant.space>
2020-11-25 11:12:22 +0000

    Check in a template for local Django settings

---
+++ review/local_settings_template.py
@@ -0,0 +1,30 @@
+import os
+
+STATIC_URL = '/static/'
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # i.e. "../"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/...")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "web/static/web"),)
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/www/...',
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '...'
+
+ALLOWED_HOSTS = ['<FQDN>']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
