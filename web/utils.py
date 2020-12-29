7fda35bbeefea3f2876ec296011453afc4f1206c
Blazej <master@constant.space>
2020-12-29 10:56:31 +0000

    Incorporate SuperMemo-2 algorithm for review pacing

--- web/utils.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-def is_due_for_review(rep_count, last_rep_date, today):
-    # review after a week, then after 2 weeks (from last review), then 4 etc.
-    return last_rep_date + datetime.timedelta(weeks=2**rep_count) <= today
