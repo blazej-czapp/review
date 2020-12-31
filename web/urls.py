589017c50accd8c263311509b71c678c48f8ffb9
Blazej Czapp <git@constant.space>
2020-12-31 12:33:32 +0000

    Allowing editing existing review items from the UI

--- web/urls.py
+++ web/urls.py
@@ -6,6 +6,8 @@ app_name = 'web'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_new_resource', views.add_new_resource, name='add_new_resource'),
+    path('edit_existing_resource', views.edit_existing_resource, name='edit_existing_resource'),
     path('reviewed', views.reviewed, name='reviewed'),
     path('review_list', views.review_list, name='review_list'),
+    path('get_raw_resource_data', views.get_raw_resource_data, name='get_raw_resource_data'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,9 +1,11 @@
+import datetime
+import json
+
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from web.models import Resource
 from django.shortcuts import redirect
 from operator import attrgetter
-import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 
@@ -24,6 +26,15 @@ def review_list(request):
     return render(request, 'web/review_list.html', {'resources': sorted(to_review, key=attrgetter('id'))})
 
 @login_required
+@require_http_methods(["GET"])
+def get_raw_resource_data(request):
+    res = Resource.objects.filter(added_by=request.user, id=request.GET['resource_id'])
+    assert res.count() == 1
+    for r in res:
+        data = { "caption": r.caption, "location": r.location, "notes": r.notes }
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+@login_required
 @require_http_methods(["POST"])
 def add_new_resource(request):
     today = datetime.date.today()
@@ -42,6 +53,29 @@ def add_new_resource(request):
 
 @login_required
 @require_http_methods(["POST"])
+def edit_existing_resource(request):
+    caption = request.POST['caption']
+
+    if not caption:
+        return HttpResponse(status=400, reason=f'caption is empty')
+
+    res_id = request.POST['resource_id']
+    res = Resource.objects.filter(added_by=request.user, id=res_id)
+    if res.count() == 0:
+        # report 400 even if res exists but is owned by a different user, there's no need to leak that information
+        return HttpResponse(status=400, reason='Attempt to edit a non existent ID: ' + res_id)
+
+    assert res.count() == 1
+    for r in res:
+        r.caption = caption
+        r.location = request.POST['location']
+        r.notes = request.POST['notes']
+        r.save()
+
+    return HttpResponse()
+
+@login_required
+@require_http_methods(["POST"])
 def reviewed(request):
     quality = int(request.POST['quality'])
     if quality < 0 or quality > 5:
@@ -52,6 +86,7 @@ def reviewed(request):
     if res.count() == 0:
         return HttpResponse(status=400, reason='Attempt to review a non existent ID: ' + res_id)
 
+    assert res.count() == 1
     for r in res:
         # Take current review's quality and infer new values for review variables:
         #  - repetitions: quality <3 breaks the streak and resets it to 1, I think
