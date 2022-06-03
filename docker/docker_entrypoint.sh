186310eaecfac1ac4182974be6257f0cd83115c9
Błażej Czapp <git@constant.space>
2022-06-03 18:20:58 +0100

    Add Dockerfile for dev server

---
+++ docker/docker_entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh --login
+
+# exec replaces the shell with the command being executed (it assumes its PID)
+exec "$@"
--- review/local_settings_template.py
+++ review/local_settings_template.py
@@ -5,7 +5,7 @@ STATIC_URL = '/static/'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # i.e. "../"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/...")
+STATIC_ROOT = "/var/www/..." # unused for dev server, unless you run collectstatic
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "web/static/web"),)
 
 # Database
@@ -21,7 +21,7 @@ DATABASES = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '...'
 
-ALLOWED_HOSTS = ['<FQDN>']
+ALLOWED_HOSTS = ['<FQDN>'] # can use ['*'] for development
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
